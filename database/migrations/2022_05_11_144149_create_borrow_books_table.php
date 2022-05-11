1aef8ea143a5630d6d44c1f34ef16b0ff34e3bb6
Supun Chalaka <supunchalaka@gmail.com>
2022-05-11 20:34:17 +0530

    Created database table ,controllers
    created library_users, borrow_books, books tables and controllers with migrations

---
+++ database/migrations/2022_05_11_144149_create_borrow_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBorrowBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('borrow_books', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('library_users')->onDelete('cascade');
+            $table->integer('book_id');
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+            $table->timestamp('borrow_date');
+            $table->integer('is_overdue')->default(0);
+            $table->timestamp('received_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('borrow_books');
+    }
+}

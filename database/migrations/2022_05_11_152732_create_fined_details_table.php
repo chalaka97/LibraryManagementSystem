00b65aa9bf4a1307da9cb9bba3e0ec9f9f7bb9ff
Supun Chalaka <supunchalaka@gmail.com>
2022-05-11 21:02:49 +0530

    fined details table created
    created fined table that contain the fined details about overdue books

---
+++ database/migrations/2022_05_11_152732_create_fined_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFinedDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fined_details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('f_user_id');
+            $table->foreign('f_user_id')->references('id')->on('library_users')->onDelete('cascade');
+            $table->integer('f_b_book_id');
+            $table->foreign('f_b_book_id')->references('id')->on('borrow_books')->onDelete('cascade');
+            $table->integer('f_days');
+            $table->double('f_total_payment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fined_details');
+    }
+}
